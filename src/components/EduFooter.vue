ef4907c3f7934cddd37247fb4069cba59e8764b6
Dario La Tegola <dariolategola@gmail.com>
2022-03-03 16:57:14 +0100

    05. Prima stilizzazione statica footer

--- src/components/EduFooter.vue
+++ src/components/EduFooter.vue
@@ -1,20 +1,86 @@
 <template>
-  <footer class="debug">
-      FOOTER
+  <footer class="debug flex">
+      <div class="debug footer-themes flex flex-col">
+          <button>Purchase</button>
+          <button>Related</button>
+      </div>
+
+      <div class="debug footer-info flex flex-col">
+          <EduLogo/>
+          <p>Lorem ipsum</p>
+          <div class="footer-icons">
+              ICONS
+          </div>
+      </div>
+
+      <div class="debug footer-nav flex">
+          <nav>
+              <h3>Get EduPrime</h3>
+              <ul>
+                  <li>Lorem</li>
+                  <li>Lorem</li>
+                  <li>Lorem</li>
+              </ul>
+          </nav>
+          <nav>
+              <h3>Networking</h3>
+              <ul>
+                  <li>Lorem</li>
+                  <li>Lorem</li>
+                  <li>Lorem</li>
+              </ul>
+          </nav>
+          
+      </div>
+
+      <div class="debug footer-search flex flex-col">
+        <input type="text" placeholder="Search...">
+        <h4>Search Categories</h4>
+        <div class="footer-tags flex">
+            <ul>
+                <li>Economy</li>
+            </ul>
+        </div>
+        <span>ModelTheme. All rights reserved.</span>
+      </div>
   </footer>
 </template>
 
 <script>
+import EduLogo from '../components/partials/EduLogo.vue'
 export default {
-    name: "EduFooter"
+    name: "EduFooter",
+    components: {
+        EduLogo,
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/style/variables.scss";
     // Inizio footer
     footer {
         height: 300px;
-        background-color: green;
+        background-color: $brandColor;
+
+        div.debug {
+            width: calc(100% / 4);
+        }
+
+        .footer-info {
+            justify-content: center;
+            align-items: center;
+        }
+
+        .footer-nav {
+            justify-content: space-around;
+            align-items: center;
+        }
+
+        .footer-search {
+            align-items: center;
+        }
+
     }
     // Fine footer
 </style>
--- src/components/EduMain.vue
+++ src/components/EduMain.vue
@@ -1,6 +1,15 @@
 <template>
   <main class="debug">
-      MAIN
+      <section class="jumbotron debug flex">
+          <div class="jumbotron-text">
+              <h1>Key to your Success</h1>
+              <p>Lorem ipsum</p>
+              <div class="jumbo-buttons">
+                  <button>Search</button>
+                  <button>Apply</button>
+              </div>
+          </div>
+      </section>
   </main>
 </template>
 
@@ -11,10 +20,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/style/variables.scss";
 // Inizio main
 main {
-    height: calc(100vh - 500px);
+    height: 100vh;
     background-color: blue;
+
+    // Inizio jumbotron
+    .jumbotron {
+        background-color: $brandColor;
+        height: 400px;
+        justify-content: center;
+        align-items: center;
+    }
+    // Fine jumbotron
 }
 // Fine main
 
